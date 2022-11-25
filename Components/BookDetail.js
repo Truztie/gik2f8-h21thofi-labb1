4da19a9e23dbd00ffc6b2aff98e5d6b75a52cefa
Thomas Finneman <thomasfinneman94@gmail.com>
2022-11-25 23:03:11 +0100

    last fixes 25nov 22

--- Components/BookDetail.js
+++ Components/BookDetail.js
@@ -1,7 +1,7 @@
 const BookDetail = (book) => {
 
     let bookDetail = `<div id=${book.id} class="book-item__detail flex bg-slate-600 text-slate-200 w-auto absolute rounded-md border-2 border-slate-900 p-4 w-full mx-auto p-">
-                        <img src="${book.coverImage}" onerror="this.style.display='none'"  width="150px" height="250px"</img>
+                        <img src="${book.coverImage}" onerror="this.style.display='none'" width="150px" height="250px"</img>
                         <div class="p-4">
                           <p class="py-2.5" > ID: ${book.id}</p>
                           <p class="py-2.5" > Title: ${book.title}</p>

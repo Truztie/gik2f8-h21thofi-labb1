27aa4790d44d55f6c327dfdfa6f61f52b405386b
Thomas Finneman <thomasfinneman94@gmail.com>
2022-11-25 22:59:45 +0100

    tailwind fixes

--- Components/BookDetail.js
+++ Components/BookDetail.js
@@ -1,21 +1,15 @@
 const BookDetail = (book) => {
 
-    let bookDetail = `<div id=${book.id} class="book-item__detail flex bg-slate-800 text-slate-200 w-auto absolute rounded-md border-2 p-4 border-slate-800 bg-white w-full mx-auto p-">
+    let bookDetail = `<div id=${book.id} class="book-item__detail flex bg-slate-600 text-slate-200 w-auto absolute rounded-md border-2 border-slate-900 p-4 w-full mx-auto p-">
                         <img src="${book.coverImage}" onerror="this.style.display='none'"  width="150px" height="250px"</img>
-                        <div class="pl-4">
-                          <p>ID: ${book.id}</p>
-                          <p>Title: ${book.title}</p>
-                          <p>Author: ${book.author}</p>
-                          <p>Pages: ${book.pages}</p>
-                          <p>Release Date: ${book.releaseDate}</p>
+                        <div class="p-4">
+                          <p class="py-2.5" > ID: ${book.id}</p>
+                          <p class="py-2.5" > Title: ${book.title}</p>
+                          <p class="py-2.5" > Author: ${book.author}</p>
+                          <p class="py-2.5" > Pages: ${book.pages}</p>
+                          <p class="py-2.5" > Release Date: ${book.releaseDate}</p>
                         </div>
                       </div>`;
     return bookDetail;
-
-  };
-  const BookDetailRemove = () => {
-
-    let bookDetailRemove = null;
-    return bookDetailRemove;
 
   };
--- Components/BookList.js
+++ Components/BookList.js
@@ -1,5 +1,5 @@
 const BookList = (bookList) => {
-  let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
+  let html = `<ul class="book-list rounded-md border-2 border-slate-900 bg-slate-600 w-full mx-auto">`;
   for (let i = 0; i < bookList.length; i++) {
     html += BookListItem(bookList[i]);
   }
